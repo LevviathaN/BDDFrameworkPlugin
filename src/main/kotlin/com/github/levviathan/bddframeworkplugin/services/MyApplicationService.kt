bd83034e51950a7a7035058dca4e7fcdacf4fa16
GitHub Action <action@github.com>
2022-04-01 09:43:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/levviathan/bddframeworkplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.levviathan.bddframeworkplugin.services
+
+import com.github.levviathan.bddframeworkplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/levviathan/bddframeworkplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/levviathan/bddframeworkplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.levviathan.bddframeworkplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.levviathan.bddframeworkplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
